5e1cfc58cf5f341528b234fbecad1506cf829a73
chiranjeevi <chiru.mahadevaiah@gmail.com>
2024-12-02 16:59:20 +0530

    docker file added

--- src/main/java/com/example/springgithub/Spring_github_project/repository/GitHubRepo.java
+++ src/main/java/com/example/springgithub/Spring_github_project/repository/GitHubRepo.java
@@ -8,6 +8,12 @@ import java.util.List;
 
 public interface GitHubRepo extends JpaRepository<GitHubModelEntity, Long> {
 
+//    select g: Selects all fields of the entity GitHubModelEntity.
+//    from GitHubModelEntity g: Specifies the entity GitHubModelEntity
+//    as the source of the query and assigns it an alias g.
+//    where g.language = ?1: Filters the results where the language field
+//    of the GitHubModelEntity matches the first parameter of the method (represented by ?1).
+//
     @Query("select g from GitHubModelEntity g where g.language = ?1")
     List<GitHubModelEntity> findByLanguage(String language);
 
